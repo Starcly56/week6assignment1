f5df02133724daa7d028b7862a67ccfda4d8fa62
Starcly56 <humagainuzjwol@gmail.com>
2021-01-12 16:52:22 +0545

    model used for the student

--- app/src/main/java/com/ujjwal/softuserreplica/model/Student.kt
+++ app/src/main/java/com/ujjwal/softuserreplica/model/Student.kt
@@ -1,46 +1,10 @@
 package com.ujjwal.softuserreplica.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class Student(
-    val name: String?,
-    val age: String?,
-    val gender: String?,
-    val mobileNumber: String?,
-    val address: String?,
-    val profilePicture: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(age)
-        parcel.writeString(gender)
-        parcel.writeString(mobileNumber)
-        parcel.writeString(address)
-        parcel.writeString(profilePicture)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Student> {
-        override fun createFromParcel(parcel: Parcel): Student {
-            return Student(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Student?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val name: String?=null,
+    val age: String?=null,
+    val gender: String?=null,
+    val mobileNumber: String?=null,
+    val address: String?=null,
+    val profilePicture: String?=null
+)
