9bd1427990079bd857d3b8c24c5697dd72205921
Starcly56 <humagainuzjwol@gmail.com>
2021-01-12 16:51:58 +0545

    adapter for recyclerview

--- app/src/main/java/com/ujjwal/softuserreplica/adapter/StudentAdapter.kt
+++ app/src/main/java/com/ujjwal/softuserreplica/adapter/StudentAdapter.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ujjwal.softuserreplica.R
@@ -12,24 +14,24 @@ import com.ujjwal.softuserreplica.model.Student
 import de.hdodenhof.circleimageview.CircleImageView
 
 class StudentAdapter(
-    val listStudent:ArrayList<Student>,
-    val context: Context
+        private val listStudent:ArrayList<Student>,
+        private val context: Context
 ) :RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
 
     class StudentViewHolder(view:View):RecyclerView.ViewHolder(view){
-        val imgProfile : CircleImageView
-        val tvName : TextView
+        val imgProfile : CircleImageView = view.findViewById(R.id.imgProfile)
+        val tvName : TextView = view.findViewById(R.id.tvName)
         val tvAddress : TextView
         val tvMobile : TextView
         val tvGender : TextView
         val tvAge : TextView
+        val trashButton : ImageButton
         init {
-            imgProfile=view.findViewById(R.id.imgProfile)
-            tvName=view.findViewById(R.id.tvName)
             tvAddress=view.findViewById(R.id.tvAddress)
             tvMobile=view.findViewById(R.id.tvMobile)
             tvGender=view.findViewById(R.id.tvGender)
             tvAge=view.findViewById(R.id.tvAge)
+            trashButton=view.findViewById(R.id.trashButton)
         }
     }
 
@@ -53,12 +55,27 @@ class StudentAdapter(
                 .into(holder.imgProfile)
         }
         else{
-            holder.imgProfile
+            when (student.gender) {
+                "Male" -> {
+                    holder.imgProfile.setBackgroundResource(R.drawable.male)
+                }
+                "Female" -> {
+                    holder.imgProfile.setBackgroundResource(R.drawable.female)
+                }
+                "Others" -> {
+                    holder.imgProfile.setBackgroundResource(R.drawable.others)
+                }
+            }
+        }
+        holder.trashButton.setOnClickListener {
+            listStudent.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, listStudent.size);
+//            Toast.makeText(context,"$position",Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun getItemCount(): Int {
         return listStudent.size
     }
-
 }
